59f0833589cbd6c171f887ce701f53ce9ef30b70
Sherin Binu <sherinbinu@hotmail.com>
2019-02-09 08:13:31 +0530

    remove navbar from homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,6 @@
 <template>
-  <div>
-    <Navbar/>
-  </div>
+  <div/>
 </template>
-
-<script>
-import Navbar from '../components/Navbar'
-
-export default {
-  components: {
-    Navbar
-  }
-}
-</script>
 
 <style>
 </style>
