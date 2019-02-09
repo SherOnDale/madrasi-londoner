a427b908823606860ec19d41233e0690c7a73a2a
Sherin Binu <sherinbinu@hotmail.com>
2019-02-09 17:09:42 +0530

    add contact item component

---
+++ components/ContactItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="contact-item">
+    <div class="contact-item__icon" v-html="svg[iconName]"/>
+    <div class="contact-item__text">{{ contactDetail }}</div>
+  </div>
+</template>
+
+<script>
+import svg from '@/assets/svg.js'
+export default {
+  props: {
+    iconName: {
+      type: String,
+      default: 'person'
+    },
+    contactDetail: {
+      type: String,
+      default: 'Contact Detail'
+    }
+  },
+  date() {
+    return {
+      svg: svg
+    }
+  }
+}
+</script>
+
+<style>
+</style>
