6dfab16c80e628f60009221262470aec0dde7736
Sherin Binu <sherinbinu@hotmail.com>
2019-02-09 08:09:40 +0530

    change design layout

---
+++ components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>navbar</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      navbarItems: [
+        {
+          name: 'home',
+          route: '/'
+        },
+        {
+          name: 'contact',
+          route: '/contact'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,17 @@
 <template>
-  <div>test</div>
+  <div>
+    <Navbar/>
+  </div>
 </template>
 
 <script>
+import Navbar from '../components/Navbar'
+
+export default {
+  components: {
+    Navbar
+  }
+}
 </script>
 
 <style>
